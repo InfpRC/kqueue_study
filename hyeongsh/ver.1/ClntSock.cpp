e6036b7e0ecd456b171b90076b6487d6c74dd01a
Hyeongseop Shin <hyeongsh@c2r3s4.42seoul.kr>
2024-07-05 22:24:47 +0900

    chore: existing file in ver.1

--- hyeongsh/ver.1/ClntSock.cpp
+++ hyeongsh/ver.1/ClntSock.cpp
@@ -1,8 +1,6 @@
 
 #include "ClntSock.hpp"
 
-ClntSock::ClntSock() {}
-
 ClntSock::ClntSock(ServSock server) : Socket() {
 	sock = accept(server.getSock(), (struct sockaddr *)&address, &address_size);
 	fcntl(sock, F_SETFL, O_NONBLOCK);
